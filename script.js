71fe511ee45c910838ae8377c4d16f14012c9377
Ben Savage <bensavage100@gmail.com>
2022-05-28 13:50:51 -0700

    corrected with openweathermap.org/appid API call

--- script.js
+++ script.js
@@ -4,5 +4,8 @@
 function weatherTime () {
 
     var cityInput = document.getElementById('yourInput');
-    
+    var bigCity = document.getElementById('yourInput');
+// fetching openweather API and getting the users input with my API Key from openweather
+ fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='+cityInput.value+'b686270c89bd1b7291dcac697927fc18') 
+ .then(respone => respone.json())  
 }
