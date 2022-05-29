9a2e28307f73cd1d673501f8cb755941948e34d2
Ben Savage <bensavage100@gmail.com>
2022-05-28 18:13:49 -0700

    working copy bare min css, local storage working

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # weathertime
+
+
+## Description 
+![Main](./assets/weatherTime.PNG)<br/>
+
+
+![WeatherID](./assets/apiJSONweatherID.PNG)<br/>
+The script.js has information on why/how this is important for getting the right icon to display and an example of how to do it. 
+
+![api](./assets/apilocation.PNG)<br/>
+First thing I did when setting this up was making sure I could make a good API call with my API key. .
+
+![get](./assets/apiGET.PNG)<br/>
+
+
+![localStorage](./assets/localStorage.PNG)<br/>
+Saving searched city into local storage.
+
+![weatherexample](./assets/weatherDayz.PNG)<br/>
+
+Example of dashed border to pin out the current days weather. 
+
+
+
+
+ 
+
+## Table of Contents 
+
+- [Description](#Description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Instructions](#Instructions)
+## Installation
+
+Just open the index.html
+
+## Usage
+
+You could use this to get the weather for the next 5 days.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)<br/>
+
+
+## Instructions
+
+Double click, index.html input whatever city you would like, just the city not the state. Like Shelton, Seattle or Dallas. 
+
+## GitHub
+
+recommend following https://www.youtube.com/c/BeginnersGuideprogramming as he has a great walkthrough 
+
+https://home.openweathermap.org/ 
+<br>
+This website has easy to follow API usage for most use cases. This is the free API access that I utilized. 
+
+recommend w3schools for easy quick local storage https://www.w3schools.com/jsref/met_storage_setitem.asp
+
+
+
+If you would like to vist my profile please feel free!<br>
+[GitHub Profile](https://github.com/savagescoles)<br>
+[![Ben](https://github.com/savagescoles.png?size=200)](https://github.com/savagescoles)
--- script.js
+++ script.js
@@ -1,11 +1,103 @@
 // My API KEY from https://home.openweathermap.org/api_keys
-// b686270c89bd1b7291dcac697927fc18
+// fa4f9a8f0b68c84289b7f6bdbef47750
 
+/*
+The following function will use the users input from the HTML, in the API call once the button is 
+clicked to get the temp (min/max) from openweathermap.org via imperial units and will cycle through 5 days
+
+*/
 function weatherTime () {
 
     var cityInput = document.getElementById('yourInput');
-    var bigCity = document.getElementById('yourInput');
-// fetching openweather API and getting the users input with my API Key from openweather
- fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='+cityInput.value+'b686270c89bd1b7291dcac697927fc18') 
- .then(respone => respone.json())  
+    var bigCity = document.getElementById('bigCity');
+    bigCity.innerHTML = cityInput.value;
+
+    localStorage.setItem('recent', bigCity.innerHTML);
+    localStorage.getItem('recent');
+
+
+
+
+// fetching openweather API and getting the users input with my API Key from openweather.org/appID documentation
+// In the API call for degrees F you must include &units=imperial as part of your API call
+ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+cityInput.value+'&units=imperial&appid=fa4f9a8f0b68c84289b7f6bdbef47750') 
+ .then(response => response.json())  
+ .then(data => {
+
+     //Getting the min and max temps for each day, top is Min and below that is the max.
+     for(w = 0; w<5; w++){
+        document.getElementById("day" + (w+1) + "Min").innerHTML = "Min: " + Number(data.list[w].main.temp_min).toFixed(1)+ "° Fahrenheit";
+        
+    }
+    //Again it's important to note that from their website, using their API the default value is Kelvin, so you must change the
+    //data request in the API call for the information you want. 
+
+    for(w = 0; w<5; w++){
+        document.getElementById("day" + (w+1) + "Max").innerHTML = "Max: " + Number(data.list[w].main.temp_max).toFixed(2) + "° Fahrenheit";
+    }
+    //------------------------------------------------------------
+
+    //Getting Weather Icons
+    // If you go to openweather.org/weather-conditions it will have all the information needed to include the icons. 
+    // In the API in the JSON the data looks like
+    /*
+    "weather": [
+        {
+            "id": 500,
+            "main": "Rain",
+            "description": "light rain"
+            "icon": "10d"
+        }
+    ]
+
+    Since it's raining currently in Shelton, the icon will be 10d so the following for loop will go to that link and generate the icon for the weather 
+    which is set to add that into the URL+ the matching icon
+    */
+     for(w = 0; w<5; w++){
+        document.getElementById("img" + (w+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[w].weather[0].icon
+        +".png";
+    }
+
+    console.log(data)
+
+
+})
+
 }
+
+function weatherTimeDefault(){
+    document.getElementById("yourInput").defaultValue = "Shelton";
+    weatherTime();
+}
+
+
+//Getting and displaying the information for the upcoming five days of the week
+var z = new Date();
+var weekday =
+     ["Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday",
+];
+
+//Function to get the correct number for the index of the days array
+function CheckDay(day){
+    if(day + z.getDay() > 6){
+        return day + z.getDay() - 7;
+    }
+    else{
+        return day + z.getDay();
+    }
+}
+
+    for(w = 0; w<5; w++){
+        document.getElementById("day" + (w+1)).innerHTML = weekday[CheckDay(w)];
+    }
+
+    function saveWeather() {
+        localStorage.setItem("searchedCity", bigCity);
+    }
